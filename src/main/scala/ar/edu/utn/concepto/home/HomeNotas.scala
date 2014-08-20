e2b55a5d1dae22af7ba014000eb23dd3da61c2d9
tomduhourq <t.duhourq@gmail.com>
2014-08-20 00:37:12 -0300

    Cambios para actualizar la materia cuando se actualiza una nota

--- src/main/scala/ar/edu/utn/concepto/home/HomeNotas.scala
+++ src/main/scala/ar/edu/utn/concepto/home/HomeNotas.scala
@@ -6,37 +6,39 @@ import ar.edu.utn.concepto.domain.Nota
 import org.joda.time.DateTime
 import org.uqbar.commons.utils.Observable
 import org.uqbar.commons.model.UserException
+import ar.edu.utn.concepto.domain.Materia
 
 @Observable
 object HomeNotas extends CollectionBasedHome[Nota] {
-	
-  def create(materia: String,fecha: DateTime, descripcion:String, aprobada: Boolean): Unit = {
+
+  def create(materia: String, fecha: DateTime, descripcion: String, aprobada: Boolean): Unit = {
     var nota = new Nota()
     nota.materia = materia
     nota.fecha = fecha
     nota.descripcion = descripcion
     nota.aprobada = aprobada
+    HomeMaterias.agregarNotaA(nota,materia)
     this.create(nota)
   }
-  
-  override def validateCreate(nota:Nota): Unit = {
+
+  override def validateCreate(nota: Nota): Unit = {
     nota.validar()
     validarNotaDuplicada(nota)
   }
-  
-  def notas: Seq[Nota]= allInstances
-  
-  def validarNotaDuplicada(nota: Nota):Unit={
+
+  def notas: Seq[Nota] = allInstances
+
+  def validarNotaDuplicada(nota: Nota): Unit = {
     val descripcion = nota.descripcion
     val materia = nota.materia
-    if (!this.search(materia,descripcion).isEmpty) {
+    if (!this.search(materia, descripcion).isEmpty) {
       throw new UserException("Ya existe una nota con descripcion: " + descripcion)
     }
   }
-  
-  def search(materia: String,descripcion: String) = 
-    notas.filter { nota => this.coincide(descripcion, nota.descripcion) && this.coincide(materia, nota.materia)}
-  
+
+  def search(materia: String, descripcion: String) =
+    notas.filter { nota => this.coincide(descripcion, nota.descripcion) && this.coincide(materia, nota.materia) }
+
   //Esto se fija si ya existía la nota por descripción. Se puede usar para cualquier tipo de parámetro.
   def coincide(expectedValue: Any, realValue: Any): Boolean = {
     if (expectedValue == null) {
@@ -47,7 +49,12 @@ object HomeNotas extends CollectionBasedHome[Nota] {
     }
     return realValue.toString().toLowerCase().contains(expectedValue.toString().toLowerCase())
   }
-  
+
+  def getNotasBy(materia: Materia) = {
+    val descripcionDeMateria = materia.nombre
+    notas.filter(nota => this.coincide(descripcionDeMateria, nota.materia))
+  }
+
   override def getEntityType = classOf[Nota]
 
   override def createExample = new Nota
